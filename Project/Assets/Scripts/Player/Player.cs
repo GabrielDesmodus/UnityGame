4f671f8280d3b43f07fb9299f3eceaa813084397
Gabriel Sá <50773849+GabrielDesmodus@users.noreply.github.com>
2020-06-22 23:29:29 -0300

    Update

--- Project/Assets/Scripts/Player/Player.cs
+++ Project/Assets/Scripts/Player/Player.cs
@@ -15,66 +15,31 @@ public class Player : MonoBehaviour
     public int noOfClicks;
     public float lastClickedTime;
     public float maxComboDelay;
-    private bool click1;
-    private bool click2;
-    private bool click3;
-    private bool clickx1;
-    private int noOfClickspe;
-    private float tP;
-    public GameObject sword_0;
-    public GameObject cc;
-    public BoxCollider2D cc1;
-    private bool atk;
-    private int dmg;
-    private Transform enemy;
-    private int dL; //Damage inflicted
-    private Transform endMarker;
-    private Transform lastMarker;
     public float iFrame; //Iframe time
-    public float force;
-    private float throwback;
     private SpriteRenderer sprite;
-    //private SpriteRenderer opacity;
-   
-    public float boundarie1, boundarie2, boundarie3, boundarie4;
-    public LayerMask layer;
-    private float waitToJump;
-    public GameObject Enemy;
-    public GameObject Enemy2;
-    private bool endOfCombo;
+    private Rigidbody2D rigidbody2D;
 
     void Start()
     {
         sprite = GetComponent<SpriteRenderer>();
-
-       
+        rigidbody2D = GetComponent<Rigidbody2D>();
         animator = spritePlayer.GetComponent<Animator>();
         vida = maxVida;
         noOfClicks = 0;
-        tP = 0;
-        sword_0 = GameObject.Find("sword_0");
-        cc = GameObject.FindGameObjectWithTag("Chao");
         iFrame = 2;
-        Enemy = GameObject.Find("EN");
-        Enemy2 = GameObject.Find("EN2");
-        GameObject[] enemies;
-        enemies = GameObject.FindGameObjectsWithTag("Enemy");
     }
 
     void Update()
     {
         iFrame += Time.deltaTime;
 
-        if(iFrame < 0.5f)
+        if(iFrame < 0.1f)
         {
             sprite.color = Color.red;
         }
         else if (iFrame < 2)
         {
             sprite.color = Color.white;
-            //var opacity = GetComponent<SpriteRenderer>().material.color;
-            
-            //opacity.a = 0.4f;
             sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, 0.5f);
             Physics2D.IgnoreLayerCollision(0, 11, true);
 
@@ -89,14 +54,6 @@ public class Player : MonoBehaviour
         lastClickedTime += Time.deltaTime;
         Attack();
         Movimentacao();
-        GroundCheck();
-        waitToJump += Time.deltaTime;
-    }
-   
-
-    void GroundCheck()
-    {
-        estaNoChao = Physics2D.OverlapCircle(chaoVerificador.position, 3, layer);
     }
 
     void Attack()
@@ -120,7 +77,6 @@ public class Player : MonoBehaviour
         }
         if (Input.GetButtonDown("Fire1") && lastClickedTime < maxComboDelay && lastClickedTime > 0.4f && noOfClicks==1)
         {
-            Debug.Log("2");
             animator.SetBool("Attack1", false);
             animator.SetBool("Attack2", true);
             lastClickedTime = 0;
@@ -128,7 +84,6 @@ public class Player : MonoBehaviour
         }
         if (Input.GetButtonDown("Fire1") && lastClickedTime < maxComboDelay && lastClickedTime > 0.4f && noOfClicks == 2)
         {
-            Debug.Log("2");
             animator.SetBool("Attack1", false);
             animator.SetBool("Attack2", false);
             animator.SetBool("Attack3", true);
@@ -144,222 +99,13 @@ public class Player : MonoBehaviour
             noOfClicks = 0;
         }
 
-
-        //if (lastClickedTime > maxComboDelay)
-        //{
-        //    noOfClicks = 0;
-        //    animator.SetBool("Attack1", false);
-        //    animator.SetBool("Attack2", false);
-        //    animator.SetBool("AttackX1", false);
-        //    animator.SetBool("Attack3", false);
-        //    lastClickedTime = 0;
-        //}
-
-        //if (endOfCombo==true || lastClickedTime >0.5f)
-        //{
-        //    animator.SetBool("Attack3", false);
-
-        //}
-
-        //if (noOfClicks == 0)
-        //{
-        //    click1 = true;
-        //    clickx1 = true;
-        //    /*click2 = false;**/
-        //    click3 = false;
-        //}
-
-        //else
-        //{
-        //    click1 = false;
-        //    clickx1 = false;
-        //}
-        //if (noOfClicks == 1)
-        //{
-        //    click2 = true;
-        //    click1 = false;
-        //    click3 = false;
-        //}
-        //else
-        //{
-        //    click2 = false;
-        //}
-
-        //if (noOfClicks == 2)
-        //{
-        //    click3 = true;
-        //    click1 = false;
-        //    /*click2 = false;*/
-        //}
-        //else
-        //{
-        //    click3 = false;
-        //}
-
-        //if (Input.GetButtonDown("Fire1") && click1 == true /*&& Time.time > tP*/ && lastClickedTime > 0.7 || endOfCombo==true)
-        //{
-        //    lastClickedTime = 0;
-        //    noOfClicks = 1;
-        //    noOfClickspe = 1;
-        //    animator.SetBool("Attack1", true);
-        //    endOfCombo = false;
-        //}
-        //else
-        //{
-        //    animator.SetBool("Attack1", false);
-        //}
-
-        //if (Input.GetButtonDown("Fire1") && click2 == true && lastClickedTime < maxComboDelay && lastClickedTime > 0.5f)
-        //{
-        //    lastClickedTime = 0;
-        //    noOfClicks = 2;
-        //    noOfClickspe = 2;
-        //    animator.SetBool("Attack2", true);
-        //}
-        //else
-        //{
-        //    /*animator.SetBool("Attack2", false);*/
-
-        //}
-
-        //if (Input.GetButtonDown("Fire1") && click3 == true && lastClickedTime <= maxComboDelay /*&& Time.time > tP*/ && lastClickedTime > 0.5f)
-        //{
-        //    Debug.Log("???");
-
-        //    animator.SetBool("Attack3", true);
-
-        //    noOfClicks = 0;
-
-        //    lastClickedTime = 0;
-        //    endOfCombo = true;
-        //}
-
-        //if (Input.GetButtonDown("Fire2") && Time.time > tP)
-        //{
-        //    animator.SetBool("AttackX1", true);
-        //    tP = Time.time + 1.35f;
-        //}
-        //if (Input.GetButton("Fire3"))
-        //{
-        //    animator.SetBool("shield", true);
-        //    /*sword_0.GetComponent<CapsuleCollider2D>().isTrigger = false;*/
-        //    velocidade = 0;
-        //}
-        //else
-        //{
-        //    animator.SetBool("shield", false);
-        //    Physics2D.IgnoreCollision(sword_0.GetComponent<CapsuleCollider2D>(), cc1.GetComponent<BoxCollider2D>());
-        //    velocidade = 14;
-        //    /*sword_0.GetComponent<CapsuleCollider2D>().isTrigger = true;*/
-        //}
-        /*else
-         {
-             animator.SetBool("AttackX1", false);
-         }
-
-         if (Input.GetButtonDown("Fire2"))
-         {
-
-             animator.SetBool("ComboXY", true);
-
-         }
-         else
-         {
-             animator.SetBool("ComboXY", false);
-         }
-
-         if (Input.GetButtonDown("Fire2") && noOfClickspe == 2)
-         {
-
-             animator.SetBool("ComboXXY", true); 
-         }
-         else
-         {
-             animator.SetBool("ComboXXY", false);
-         }*/
     }
-
-    //void Movimentacao()
-    //{
-    //    var rigidbody2D = GetComponent<Rigidbody2D>();
-    //    rigidbody2D.velocity = Vector2.ClampMagnitude(rigidbody2D.velocity, 80);
-    //    if (!estaNoChao)
-    //    {
-    //        rigidbody2D.gravityScale = 40;
-
-    //    }
-    //    else
-    //    {
-    //        rigidbody2D.gravityScale = 5;
-
-    //    }
-
-    //    if (Input.GetAxisRaw("Horizontal") > 0 && iFrame > 0.25f && waitToJump > 0.25f && estaNoChao)
-    //    {
-    //        if (estaNoChao)
-    //        {
-    //            transform.eulerAngles = new Vector2(0, 0);
-
-    //            rigidbody2D.MovePosition(transform.position + transform.right * 16 * Time.fixedDeltaTime);
-
-    //        }
-    //    }
-    //    if (waitToJump > 0.25f && Input.GetAxisRaw("Horizontal") > 0 && iFrame > 0.25f && Input.GetButtonDown("Jump") && estaNoChao)
-    //    {
-    //        waitToJump = 0;
-    //        transform.eulerAngles = new Vector2(0, 0);
-    //        rigidbody2D.velocity = Vector2.ClampMagnitude(rigidbody2D.velocity, 120);
-    //        rigidbody2D.velocity = rigidbody2D.velocity + Vector2.up * 80;
-    //        rigidbody2D.velocity = rigidbody2D.velocity + Vector2.right * 30;
-    //        rigidbody2D.velocity = Vector2.ClampMagnitude(rigidbody2D.velocity, 80);
-
-
-    //    }
-
-    //    if (Input.GetAxisRaw("Horizontal") < 0 && iFrame > 0.25f && waitToJump>0.25f)
-    //    {
-    //        if (estaNoChao)
-    //        {
-    //            transform.eulerAngles = new Vector2(0, 180);
-
-    //            rigidbody2D.MovePosition(transform.position + transform.right * 16 * Time.fixedDeltaTime);
-
-    //        }
-    //    }
-    //    if (waitToJump >0.25f && Input.GetAxisRaw("Horizontal") < 0 && iFrame > 0.25f && Input.GetButtonDown("Jump") && estaNoChao)
-    //    {
-    //        waitToJump =0;
-    //        transform.eulerAngles = new Vector2(0, 180);
-    //        rigidbody2D.velocity = rigidbody2D.velocity + Vector2.up * 80;
-    //        rigidbody2D.velocity = rigidbody2D.velocity + Vector2.left * 30;
-
-
-    //    }
-    //    else if (Input.GetAxisRaw("Horizontal") == 0 && Input.GetButtonDown("Jump") && estaNoChao && iFrame > 1)
-    //    {
-
-    //        rigidbody2D.velocity = rigidbody2D.velocity + Vector2.up * 60;
-    //        //rigidbody2D.AddForce(transform.up * 7000);
-    //    }
-
-
-
-
-
-
-    //    //estaNoChao = Physics2D.Linecast(transform.position, chaoVerificador.position, 1 << LayerMask.NameToLayer("Piso"));
-    //    animator.SetFloat("movimento", Mathf.Abs(Input.GetAxisRaw("Horizontal")));
-    //    //estaNoChao = Physics2D.Linecast(transform.position, chaoVerificador.position, 1 << LayerMask.NameToLayer("Piso"));
-    //    animator.SetBool("chao", estaNoChao);
-    //}
-
-
 
 
     //The enemy calls this function to deal damage to the player
     void Movimentacao()
     {
-        var rigidbody2D = GetComponent<Rigidbody2D>();
+        
 
         if (!estaNoChao)
         {
