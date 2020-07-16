199f455414fffd6ea638b690ff9bca5cb402288c
Gabriel Sá <50773849+GabrielDesmodus@users.noreply.github.com>
2020-07-15 23:50:41 -0300

    Update

--- Project/Assets/Scripts/EN/Controller.cs
+++ Project/Assets/Scripts/EN/Controller.cs
@@ -11,7 +11,7 @@ public class Controller : MonoBehaviour
     public Vector3 position2;
     public Vector3 position3;
     private int qty;
-    private int wave=0;
+    private int wave=4;
     List<GameObject> listOfOpponents = new List<GameObject>();
     
     public void EnemyAdd()
@@ -22,11 +22,9 @@ public class Controller : MonoBehaviour
     public void EnemyRemov()
     {
         qty--;
-        Debug.Log(qty);
   
         if (qty<=0)
         {
-            Debug.Log("YES");
             InstantEnemy();
         }
     }
@@ -41,12 +39,27 @@ public class Controller : MonoBehaviour
                 Instantiate(enemy2, new Vector3(4.74f, 30.84f, -22), Quaternion.Euler(0, 0, 0));
                 Instantiate(enemy1s, new Vector3(-35, 5, -22), transform.rotation);
                 break;
+
             case 2:
                 Instantiate(enemy, new Vector3(14.2f, 9.59f, -22), Quaternion.Euler(0, 180, 0));
                 Instantiate(enemy, new Vector3(-1.17f, 9.59f, -22), Quaternion.Euler(0, 0, 0));
                 Instantiate(enemy, new Vector3(-35.87f, 9.59f, -22), transform.rotation);
                 break;
 
+            case 3:
+                Instantiate(enemy1s, new Vector3(10.03f, 14.16f, -22), Quaternion.Euler(0, 180, 0));
+                Instantiate(enemy1s, new Vector3(-38.44f, 9.59f, -22), Quaternion.Euler(0, 0, 0));
+                break;
+
+            case 4:
+                Instantiate(enemy1s, new Vector3(-12.57f, 17.39f, -22), Quaternion.Euler(0, 180, 0));
+                break;
+
+            case 5:
+                Instantiate(enemy1s, new Vector3(18.03f, 9, -22), Quaternion.Euler(0, 180, 0));
+                Instantiate(enemy1s, new Vector3(-39.78f, 9, -22), Quaternion.Euler(0, 180, 0));
+                Instantiate(enemy2, new Vector3(-11.1f, 24.16f, -22), Quaternion.Euler(0, 0, 0));
+                break;
         }
         
     }
