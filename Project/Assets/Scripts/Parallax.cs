e498101125910116984e2dbdbda67c43832cedd8
Gabriel Sá <50773849+GabrielDesmodus@users.noreply.github.com>
2020-07-21 23:50:55 -0300

    Update

--- Project/Assets/Scripts/Parallax.cs
+++ Project/Assets/Scripts/Parallax.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
+    private Transform player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector2.right * (transform.position.z * 0.5f) * Time.deltaTime);
     }
 }
