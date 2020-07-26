bc4d9274ec2d56ee0601ed5d568dbf3970cec037
Gabriel Sá <50773849+GabrielDesmodus@users.noreply.github.com>
2020-07-25 23:29:01 -0300

    Update

--- Project/Assets/Scripts/Camera/MCamera.cs
+++ Project/Assets/Scripts/Camera/MCamera.cs
@@ -22,33 +22,22 @@ public class MCamera : MonoBehaviour {
     private float horizontalMax;
     private Vector3 novaPosicao;
     private float halfHeight;
-    private float halfWidth;
     private float camera_bigger;
     void Start () {
 
         camera = Camera.main;
         player = GameObject.FindGameObjectWithTag("Player").transform;
 
-        Debug.Log(camera.aspect);
-        Debug.Log(Screen.width / Screen.height);
-        if (camera.aspect <= 1.779556f)
-        {
-            float value = (1.779557f - camera.aspect)/ 1.779557f;
-                
-            //camera.orthographicSize = camera.orthographicSize+(camera.orthographicSize * value);
-            //camera.aspect = 1.779557f;
-        }
-
         halfHeight = camera.orthographicSize;
-        halfWidth = camera.aspect * halfHeight;
+        horizontalMax = camera.aspect * halfHeight;
 
         parallax = GameObject.FindGameObjectWithTag("parallax").transform.GetComponent<Parallax>();
-        horizontalMax = halfWidth;
-        if(max - mix <= (horizontalMax * 2))
-        {
+        //if(max - mix <= (horizontalMax * 2))
+        //{
+        //    camera_bigger = true;
+        //}
 
-        }
-        if(transform.position.x < mix + horizontalMax)
+        if (transform.position.x < mix + horizontalMax)
         {
             transform.position = new Vector3(mix + horizontalMax, transform.position.y, transform.position.z);
         }
@@ -57,32 +46,25 @@ public class MCamera : MonoBehaviour {
         {
             transform.position = new Vector3(max + horizontalMax, transform.position.y, transform.position.z);
         }
-
         parallax.Reset();
     }
 
-	void Update () {
+	void Update()
+    {
+      
+        Vector3 novaPosicao = new Vector3(player.position.x, player.position.y, transform.position.z);
+        transform.position = novaPosicao;
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, (mix + horizontalMax), max - horizontalMax),transform.position.y,transform.position.z);
 
-
-        //transform.position = Vector3.Lerp(transform.position, novaPosicao, Time.time);//
-        Debug.Log(max - mix); 
-        Debug.Log(horizontalMax); 
-
-        if (max-mix <= (horizontalMax * 2))
+        if (transform.position.x < mix + horizontalMax)
         {
-            Vector3 novaPosicao = new Vector3(max - ((max-mix)/2), player.position.y, transform.position.z);
-            transform.position = novaPosicao;
-            Debug.Log("YES");
-            
+            transform.position = new Vector3(mix + horizontalMax, transform.position.y, transform.position.z);
         }
-        else
+
+        if (transform.position.x > max + horizontalMax)
         {
-            Debug.Log("NOOOOOO");
-            Vector3 novaPosicao = new Vector3(player.position.x, player.position.y, transform.position.z);
-            transform.position = novaPosicao;
-            transform.position = new Vector3(Mathf.Clamp(transform.position.x, (mix + horizontalMax), max - horizontalMax),transform.position.y,transform.position.z);
+            transform.position = new Vector3(max + horizontalMax, transform.position.y, transform.position.z);
         }
-       
 
     }
 }
